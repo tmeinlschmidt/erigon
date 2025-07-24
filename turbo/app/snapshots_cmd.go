5b32f0d2c235a44851ad2cb173e1928dd044c7be
awskii <artem.tsskiy@gmail.com>
2025-07-24 17:04:30 +0100

    save

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -1876,8 +1876,6 @@ func doRetireCommand(cliCtx *cli.Context, dirs datadir.Dirs) error {
 	defer logger.Info("Done")
 	ctx := cliCtx.Context
 
-	from := uint64(0)
-
 	db := dbCfg(kv.ChainDB, dirs.Chaindata).MustOpen()
 	defer db.Close()
 	chainConfig := fromdb.ChainConfig(db)
@@ -1921,12 +1919,10 @@ func doRetireCommand(cliCtx *cli.Context, dirs datadir.Dirs) error {
 	blockReader, _ := br.IO()
 
 	blocksInSnapshots := blockReader.FrozenBlocks()
-
 	if chainConfig.Bor != nil {
 		blocksInSnapshots = min(blocksInSnapshots, blockReader.FrozenBorBlocks(false))
 	}
-
-	if err := br.RetireBlocks(ctx, from, to, log.LvlInfo, nil, nil, nil); err != nil {
+	if err := br.RetireBlocks(ctx, blocksInSnapshots, to, log.LvlInfo, nil, nil, nil); err != nil {
 		return err
 	}
 
