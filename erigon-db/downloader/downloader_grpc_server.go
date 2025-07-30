2c7e64a2bc5cf44bf887926d591d7f4ace7d262b
alex <AskAlexSharov@gmail.com>
2025-07-30 10:59:44 +0700

    save

--- erigon-db/downloader/downloader_grpc_server.go
+++ erigon-db/downloader/downloader_grpc_server.go
@@ -106,7 +106,7 @@ func (s *GrpcServer) Add(ctx context.Context, request *proto_downloader.AddReque
 				return nil, err
 			}
 		}
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 	}
 	s.d.afterAdd()
 	progress.Store(int32(len(request.Items)))
