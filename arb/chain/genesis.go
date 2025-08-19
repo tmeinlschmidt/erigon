9285e0d940ac14b19c5ab38d0e306d402ad05f8c
awskii <artem.tsskiy@gmail.com>
2025-08-20 00:12:35 +0100

    save

--- arb/chain/genesis.go
+++ arb/chain/genesis.go
@@ -34,15 +34,15 @@ func Arb1RollupGenesisBlock() *types.Genesis {
 	return &types.Genesis{
 		Config:     Arb1ChainConfig,
 		Nonce:      0x0000000000000001,
-		Timestamp:  0x0,
+		Timestamp:  0x630f8216, //0x0,
 		ExtraData:  common.FromHex("0x0000000000000000000000000000000000000000000000000000000000000000"),
 		GasLimit:   0x4000000000000, // as given in hex
 		Difficulty: big.NewInt(1),   // "0x1"
 		Mixhash:    common.HexToHash("0x00000000000000000000000000000000000000000000000a0000000000000000"),
 		Coinbase:   common.HexToAddress("0x0000000000000000000000000000000000000000"),
-		Number:     0x0, // block number 0
-		GasUsed:    0x0,
-		ParentHash: common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+		Number:     22207818,
+		GasUsed:    0x0, // 0xe1d88
+		ParentHash: common.HexToHash("0x7d237dd685b96381544e223f8906e35645d63b89c19983f2246db48568c07986"),
 		BaseFee:    big.NewInt(0x5f5e100),
 		Alloc:      chainspec.ReadPreallocDirectly("arb/chain/hugeallocs/arb1.json"),
 	}
