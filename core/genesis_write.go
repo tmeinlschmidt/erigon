fc28a490358d408b03d19817d642ddae06f14e07
awskii <artem.tsskiy@gmail.com>
2025-08-20 00:22:44 +0100

    save

--- core/genesis_write.go
+++ core/genesis_write.go
@@ -220,9 +220,9 @@ func WriteGenesisState(g *types.Genesis, tx kv.RwTx, dirs datadir.Dirs, logger l
 
 	stateWriter := state.NewNoopWriter()
 
-	if block.Number().Sign() != 0 {
-		return nil, statedb, errors.New("can't commit genesis block with number > 0")
-	}
+	// if block.Number().Sign() != 0 {
+	// 	return nil, statedb, errors.New("can't commit genesis block with number > 0")
+	// }
 	if err := statedb.CommitBlock(&chain.Rules{}, stateWriter); err != nil {
 		return nil, statedb, fmt.Errorf("cannot write state: %w", err)
 	}
