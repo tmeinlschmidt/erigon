f1d5a3f297822efc9c26c3a4753e708fd016e312
awskii <artem.tsskiy@gmail.com>
2025-08-20 01:27:51 +0100

    save

--- arb/chain/genesis.go
+++ arb/chain/genesis.go
@@ -40,7 +40,7 @@ func Arb1RollupGenesisBlock() *types.Genesis {
 		Difficulty: big.NewInt(1),   // "0x1"
 		Mixhash:    common.HexToHash("0x00000000000000000000000000000000000000000000000a0000000000000000"),
 		Coinbase:   common.HexToAddress("0x0000000000000000000000000000000000000000"),
-		Number:     22207818,
+		Number:     22207817,
 		GasUsed:    0x0, // 0xe1d88
 		ParentHash: common.HexToHash("0x7d237dd685b96381544e223f8906e35645d63b89c19983f2246db48568c07986"),
 		BaseFee:    big.NewInt(0x5f5e100),
